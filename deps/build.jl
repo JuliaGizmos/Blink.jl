96b7b38d48a077be7ca8bfef6b76afba39a6509b
Mike Innes <mike.j.innes@gmail.com>
2015-01-04 10:25:06 -0800

    fix linux build step

--- deps/build.jl
+++ deps/build.jl
@@ -4,18 +4,13 @@ version = "0.20.3"
 
 download(x) = run(`curl -LO $x`)
 
-@unix_only unzip(x) = run(`unzip $x`)
-
-@windows_only unzip(x, y) =
-  run(`7z x $x -o$y`)
-
 download("http://junolab.s3.amazonaws.com/atom-shell/julia.png")
 
 @osx_only begin
   rm′("Julia.app")
   shell = "atom-shell-mac-$version.zip"
   download("http://junolab.s3.amazonaws.com/atom-shell/$shell")
-  unzip(shell)
+  run(`unzip $x`)
   rm(shell)
 end
 
@@ -24,13 +19,15 @@ end
   shell = "atom-shell-v$version-win32-ia32.zip"
   download("https://github.com/atom/atom-shell/releases/download/v$version/$shell")
   unzip(shell, "atom")
+  run(`7z x $shell -oatom`)
   rm(shell)
 end
 
 @linux_only begin
+  rm′("atom")
   arch = Int == Int64 ? "x64" : "ia32"
   shell = "atom-shell-v$version-linux-$arch.zip"
   download("https://github.com/atom/atom-shell/releases/download/v$version/$shell")
-  unzip(shell)
-  rm(shell)
+  run(`unzip $shell -d atom`)
+  #rm(shell)
 end
