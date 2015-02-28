65a07721d9ed40eda87f4a8816a3c32257ff1c98
Mike Innes <mike.j.innes@gmail.com>
2015-02-28 21:58:40 +0000

    organisation

---
+++ src/content/config.jl
@@ -0,0 +1,9 @@
+port() = get(ENV, "BLINK_PORT", 80)
+
+const ippat = r"([0-9]+\.){3}[0-9]+"
+
+@unix_only localips() =
+  map(IPv4, readlines(`ifconfig` |>
+                      `grep -Eo $("inet (addr:)?$(ippat.pattern)")` |>
+                      `grep -Eo $(ippat.pattern)` |>
+                      `grep -v $("127.0.0.1")`))
--- src/content/content.jl
+++ src/content/content.jl
@@ -1,16 +1,6 @@
 module Content
 
-# Configuration
-
-port() = get(ENV, "BLINK_PORT", 80)
-
-const ippat = r"([0-9]+\.){3}[0-9]+"
-
-@unix_only localips() =
-  map(IPv4, readlines(`ifconfig` |>
-                      `grep -Eo $("inet (addr:)?$(ippat.pattern)")` |>
-                      `grep -Eo $(ippat.pattern)` |>
-                      `grep -v $("127.0.0.1")`))
+# Content
 
 # Server Setup
 
@@ -35,7 +25,5 @@ function sock_handler(req, client)
     write(client, msg)
   end
 end
-
-# Content
 
 end
