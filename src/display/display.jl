a174c15a1127d317a0c8bdc04703b92109799466
Mike Innes <mike.j.innes@gmail.com>
2015-03-08 13:05:44 +0000

    more robust last-window bit

--- src/display/display.jl
+++ src/display/display.jl
@@ -1,20 +1,26 @@
 export pin, top
 
 type WebView
-  last
+  ws
   pinned
 end
 
 pinned(view::WebView) =
   isa(view.pinned, Window) && active(view.pinned) ? view.pinned : nothing
 
-WebView() = WebView(nothing, nothing)
+function last(view::WebView)
+  while !isempty(view.ws) && !active(view.ws[end]) pop!(view.ws) end
+  isempty(view.ws) ? nothing : view.ws[end]
+end
+
+WebView() = WebView(c(), nothing)
 
 const _display = WebView()
 view() = _display
 
 function init()
   setdisplay(Media.Graphical, view())
+  return
 end
 
 displaysize(x) = (500, 400)
@@ -23,13 +29,12 @@ displaytitle(x) = "Julia"
 function Media.render(view::WebView, x; options = @d())
   size = displaysize(x)
   html = tohtml(x)
-  w = isa(pinned(view), Window) ? view.pinned :
-        Window(@d(:width => size[1], :height => size[2]))
+  w = @or(pinned(view), Window(@d(:width => size[1], :height => size[2])))
+  front(w)
   body(w, html)
   title(w, string(displaytitle(x), " (", id(w), ")",
                   isa(pinned(view), Window) ? pinstr : ""))
-  view.last = w
-  front(w)
+  push!(view.ws, w)
   return w
 end
 
@@ -44,22 +49,23 @@ pintitle(::Nothing) = nothing
 unpintitle(::Nothing) = nothing
 
 function pin(w::Window)
-  if view().pinned == w
+  if pinned(view()) == w
     pin(nothing)
   elseif active(w)
     pin(nothing)
     pintitle(w)
     view().pinned = w
+    push!(view().ws, w)
   end
   return
 end
 
 # pin(id) = pin(Window(id, shell()))
-pin() = pin(view().last)
+pin() = pin(last(view()))
 
 function pin(::Nothing)
-  if view().pinned ≠ nothing
-    unpintitle(view().pinned)
+  if pinned(view()) ≠ nothing
+    unpintitle(pinned(view()))
     view().pinned = nothing
   end
 end
@@ -72,4 +78,4 @@ top(w::Window) = (floating(w, !floating(w)); nothing)
 
 top(::Nothing) = nothing
 
-top() = top(view().last)
+top() = top(last(view()))
