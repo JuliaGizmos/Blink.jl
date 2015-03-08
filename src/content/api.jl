2ac087afc7f8211022b8dcc9d6f3fd9a6beeca69
Mike Innes <mike.j.innes@gmail.com>
2015-03-08 22:01:23 +0000

    add content api

--- src/content/api.jl
+++ src/content/api.jl
@@ -1,4 +1,6 @@
-export body
+export body, content!
 
-body(o, html::String) =
-  @js_ o document.body.innerHTML = $html
+content!(o, sel, html::String) =
+  @js_ o document.querySelector($sel).innerHTML = $html
+
+body(w, html) = content!(w, "body", html)
