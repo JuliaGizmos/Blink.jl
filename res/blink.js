711019943b5175e6d4e040f9315b98353378ea0b
Mike Innes <mike.j.innes@gmail.com>
2015-03-19 22:30:29 +0000

    modularise blink.js

--- res/blink.js
+++ res/blink.js
@@ -1,29 +1,38 @@
-var ws = location.href.replace("http", "ws");
-var sock = new WebSocket(ws);
+(function() {
 
-function msg(t, m) {
-  if (m == undefined) {
-    m = t;
-  } else {
-    m.type = t;
+  var ws = location.href.replace("http", "ws");
+  var sock = new WebSocket(ws);
+
+  function msg(t, m) {
+    if (m == undefined) {
+      m = t;
+    } else {
+      m.type = t;
+    }
+    sock.send(JSON.stringify(m));
   }
-  sock.send(JSON.stringify(m));
-}
 
-var handlers = {};
+  var handlers = {};
 
-handlers.eval = function(data) {
-  var result = eval(data.code);
-  if (data.callback) {
-    result == undefined && (result = null);
-    result = {type: 'callback', callback: data.callback, result: result};
-    sock.send(JSON.stringify(result));
+  handlers.eval = function(data) {
+    var result = eval(data.code);
+    if (data.callback) {
+      result == undefined && (result = null);
+      result = {type: 'callback', callback: data.callback, result: result};
+      sock.send(JSON.stringify(result));
+    }
   }
-}
 
-sock.onmessage = function (event) {
-  var msg = JSON.parse(event.data);
-  if (handlers.hasOwnProperty(msg.type)) {
-    handlers[msg.type](msg);
+  sock.onmessage = function (event) {
+    var msg = JSON.parse(event.data);
+    if (handlers.hasOwnProperty(msg.type)) {
+      handlers[msg.type](msg);
+    }
+  };
+
+  Blink = {
+    sock: sock,
+    msg: msg,
+    handlers: handlers
   }
-};
+})();
