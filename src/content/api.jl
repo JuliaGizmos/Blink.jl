87be372ebb007ecd6da0c7859404832abfe3ec22
Mike Innes <mike.j.innes@gmail.com>
2015-03-22 14:57:31 +0000

    add loadjs and loadcss api

--- src/content/api.jl
+++ src/content/api.jl
@@ -1,4 +1,4 @@
-export body!, content!
+export body!, content!, loadcss!, loadjs!
 
 content!(o, sel, html::String) =
   @js_ o Blink.fill($sel, $html)
@@ -7,3 +7,24 @@ content!(o, sel, html) =
   content!(o, sel, stringmime(MIME"text/html"(), html))
 
 body!(w, html) = content!(w, "body", html)
+
+function loadcss!(w, url)
+  @js_ w begin
+    @var link = document.createElement("link")
+    link.type = "text/css"
+    link.rel = "stylesheet"
+    link.href = $url
+    document.head.appendChild(link)
+  end
+end
+
+function loadjs!(w, url)
+  id, cb = callback!()
+  @js_ w begin
+    @var script = document.createElement("script")
+    script.src = $url
+    script.onload = e -> Blink.cb($id)
+    document.head.appendChild(script)
+  end
+  return wait(cb)
+end
