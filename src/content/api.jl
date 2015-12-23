9d220dd1c74dfebed69cefe3756b5fa3d87a1998
Eric Forgy <Eric.Forgy@coherent.com.hk>
2015-12-23 20:14:55 +0800

    Add blink.js to existing page

--- src/content/api.jl
+++ src/content/api.jl
@@ -47,3 +47,25 @@ function load!(w, file)
     error("Blink: Unsupported file type")
   end
 end
+
+function addblink!(w)
+    @app blinkserver = (
+    Mux.defaults,
+    page("/blink.js",respond(open(readall,Pkg.dir("Blink","res","blink.js")))),
+    Mux.notfound())
+    serve(blinkserver)
+
+    varid = "var id = $(w.id)"
+    ws = "ws://127.0.0.1:$(Blink.port)/$(w.id)"
+    @js_ w begin
+        varid = document.createElement("script")
+        varid.type = "text/javascript";
+        varid.innerText = $varid
+        document.head.appendChild(varid);
+        blinkjs = document.createElement("script");
+        blinkjs.type = "text/javascript";
+        blinkjs.src = "http://localhost:8000/blink.js";
+        blinkjs.onload = e -> (Blink.sock = @new WebSocket($ws))
+        document.head.appendChild(blinkjs);
+    end
+end
