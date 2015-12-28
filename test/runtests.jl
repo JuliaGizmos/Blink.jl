5e9b58653e9ccf69f88f2d30a455bd550f790966
Spencer Lyon <spencerlyon2@gmail.com>
2015-12-28 10:22:01 -0700

    adding no show to Window() in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using Blink
 using Base.Test
 
 # open window and wait for it to initialize
-w = Window(); sleep(5.0)
+w = Window(Blink.@d(:show => false)); sleep(5.0)
 
 # make sure the window is really active
 @test_approx_eq @js(w, Math.log(10)) log(10)
