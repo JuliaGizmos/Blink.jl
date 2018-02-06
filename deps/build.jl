b17c145fa9d41a0d5575e7be6a3f72c06c93080c
Spencer Lyon <spencerlyon2@gmail.com>
2018-02-05 20:35:21 -0500

    CLN: wrap Blink build build.jl in module b/c some global variables get defined...

--- deps/build.jl
+++ deps/build.jl
@@ -1,10 +1,17 @@
+module BuildBlink
+# put into module b/c some globals are defined in install.jl
+
 include(joinpath(@__DIR__, "../src/AtomShell/install.jl"))
 
 function get_installed_version()
-    _path = is_apple() ? joinpath(folder(), "version") : joinpath(folder(), "atom", "version")
+    _path = is_apple() ?
+        joinpath(folder(), "version") :
+        joinpath(folder(), "atom", "version")
     strip(readstring(_path), 'v')
 end
 
 if isinstalled() && !(version == get_installed_version())
     Blink.AtomShell.install()
+end
+
 end
