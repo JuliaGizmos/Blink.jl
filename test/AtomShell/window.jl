696d36970f38e65466d361ffa9cd9327ec48a38f
Nathan Daly <nhdaly@gmail.com>
2018-02-20 11:15:25 -0600

    reduced window size for size test to fix xvfb-run test.

--- test/AtomShell/window.jl
+++ test/AtomShell/window.jl
@@ -2,8 +2,8 @@ using Blink
 using Base.Test
 
 @testset "size Tests" begin
-    w = Window(Blink.@d(:show => false, :width=>800, :height=>400)) ; sleep(5.0);
-    @test size(w) == [800,400]
+    w = Window(Blink.@d(:show => false, :width=>150, :height=>100)) ; sleep(5.0);
+    @test size(w) == [150,100]
 
     size(w, 100,200)
     @test size(w) == [100,200]
