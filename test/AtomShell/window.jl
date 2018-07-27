aaa371e3cd87d5d50e0b7770cdb6b9b8cc1655ce
Nathan Daly <nhdaly@gmail.com>
2018-07-27 16:38:19 -0400

    Fix Appveyor Windows test failure: win was too small

--- test/AtomShell/window.jl
+++ test/AtomShell/window.jl
@@ -5,6 +5,6 @@ using Base.Test
     w = Window(Blink.@d(:show => false, :width=>150, :height=>100)) ; sleep(5.0);
     @test size(w) == [150,100]
 
-    size(w, 100,200)
-    @test size(w) == [100,200]
+    size(w, 200,200)
+    @test size(w) == [200,200]
 end
