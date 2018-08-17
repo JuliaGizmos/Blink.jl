1e7a678857f750e7400a353317c43d2ba70b6287
Robin Deits <robin.deits@gmail.com>
2018-08-17 12:32:58 -0400

    make install.jl more standalone

--- src/AtomShell/install.jl
+++ src/AtomShell/install.jl
@@ -1,4 +1,5 @@
 import BinDeps
+using Compat
 
 rm′(f) = (isdir(f) || isfile(f)) && rm(f, recursive = true)
 
@@ -6,20 +7,20 @@ const version = "2.0.5"
 
 folder() = normpath(joinpath(@__FILE__, "../../../deps"))
 
-@static if isapple()
+@static if Compat.Sys.isapple()
     uninstall() = map(rm′, filter(x -> !endswith(x, "build.jl"), readdir(folder())))
 else
     uninstall() = rm′(joinpath(folder(), "atom"))
 end
 
-isinstalled() = isapple() ?
+isinstalled() = Compat.Sys.isapple() ?
     isfile(joinpath(folder(), "version")) :
     isdir(joinpath(folder(), "atom"))
 
 
 function install()
   dir = folder()
-  if isapple()
+  if Compat.Sys.isapple()
     _icons = normpath(joinpath(@__FILE__, "../../../res/julia-icns.icns"))
   end
   !isdir(dir) && mkpath(dir)
@@ -29,7 +30,7 @@ function install()
 
     download("http://junolab.s3.amazonaws.com/blink/julia.png")
 
-    if isapple()
+    if Compat.Sys.isapple()
       file = "electron-v$version-darwin-x64.zip"
       download("https://github.com/electron/electron/releases/download/v$version/$file")
       run(`unzip -q $file`)
@@ -41,7 +42,7 @@ function install()
       run(`touch Julia.app`)  # Apparently this is necessary to tell the OS to double-check for the new icons.
     end
 
-    if iswindows()
+    if Compat.Sys.iswindows()
       arch = Int == Int64 ? "x64" : "ia32"
       file = "electron-v$version-win32-$arch.zip"
       download("https://github.com/electron/electron/releases/download/v$version/$file")
@@ -49,7 +50,7 @@ function install()
       rm(file)
     end
 
-    if islinux()
+    if Compat.Sys.islinux()
       arch = Int == Int64 ? "x64" : "ia32"
       file = "electron-v$version-linux-$arch.zip"
       download("https://github.com/electron/electron/releases/download/v$version/$file")
