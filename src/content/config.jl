7c417cf2c6461c0619d2ab20efa163baffc941d3
Travis DePrato <773453+travigd@users.noreply.github.com>
2018-10-07 23:15:31 -0400

    Fix incorrect get(ENV, ...) (#165)
    * Update config.jl

--- src/content/config.jl
+++ src/content/config.jl
@@ -2,7 +2,7 @@ export localips
 
 const port = Ref{Int}()
 
-@init port[] = haskey(ENV, "BLINK_PORT") ? parse(Int, get(ENV, "BLINK_PORT")) : rand(2_000:10_000)
+@init port[] = haskey(ENV, "BLINK_PORT") ? parse(Int, ENV["BLINK_PORT"]) : rand(2_000:10_000)
 
 const ippat = r"([0-9]+\.){3}[0-9]+"
 
